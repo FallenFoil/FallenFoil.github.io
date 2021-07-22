a2855682857ba451cc1ff4b6cf58de708d1be840
Luis Macedo <luisjrsm@hotmail.com>
2021-07-22 15:42:09 +0100

    New icon

--- website/src/Components/Header.js
+++ website/src/Components/Header.js
@@ -6,7 +6,8 @@ class Header extends Component {
    constructor(props){
       super(props);
       this.state = {
-         is_preloaded: true
+         is_preloaded: true,
+         first_render: true
       };
    }
    componentDidMount(){
@@ -18,17 +19,38 @@ class Header extends Component {
          var name = this.props.data.name;
          var descriptions = this.props.data.description;
 
+         var delay = false;
+         if(this.state.first_render){
+            this.state.first_render = false;
+            this.setState({...this.state});
+            delay = true;
+         }
+         
          var type_descriptions = <TypistLoop interval={100}>
-                                    {descriptions.map(text => <Typist className="typist" key={text} startDelay={500}>{text}<Typist.Backspace count={text.length} delay={1000} /></Typist>)}
-                                 </TypistLoop>  
+                                 {descriptions.map((text, i) => 
+                                    <Typist className="typist" key={text} startDelay={500}>
+                                       {delay && i===0 && <Typist.Delay ms={1439}/>}
+                                       {text}
+                                       <Typist.Backspace count={text.length} delay={1000}/>
+                                    </Typist>
+                                 )}
+                              </TypistLoop>
 
          var networks= this.props.data.social.map(function(network){
             return <li key={network.name}><a href={network.url} target="_blank" rel="noreferrer"><i className={network.className}></i></a></li>
          })
       }
 
+      var stars = [];
+      for(var i=1; i<10; i++){
+         stars.push(<span className={"light x"+i}></span>)
+      }
+
       return (
          <header id="home" className={this.state.is_preloaded ? 'is-preload' : ''}>
+            {/*<span className="night">
+               {stars}
+      </span>*/}
             <nav id="nav-wrap">
                <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
                <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
@@ -44,7 +66,7 @@ class Header extends Component {
             <div className="row banner">
                <div className="banner-text">
                   <h1 className="responsive-headline name-animation">{name}</h1>
-                  {type_descriptions}
+                  <div className="description-animation">{type_descriptions}</div>
                   <hr />
                   <ul className="social">
                      {networks}
