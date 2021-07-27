c0dab05f92c3c5e3be7114781af3feb2a48e0105
Luis Macedo <luisjrsm@hotmail.com>
2021-07-27 15:33:59 +0100

    Updates

--- website/src/Components/Portfolio.js
+++ website/src/Components/Portfolio.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
+import HoverVideoPlayer from 'react-hover-video-player';
 
 class Portfolio extends Component {
   render() {
     if(this.props.data){
-      var projects = this.props.data.projects.map(function(projects){
+      var projects = this.props.data.projects.map(function(projects, i){
         var projectImage = 'images/portfolio/'+projects.image;
         return <div key={projects.title} className="columns portfolio-item">
            <div className="item-wrap">
             <a href={projects.url} title={projects.title}>
-                {projects.video ? <video autoPlay loop muted>
-                  <source src={projectImage} type="video/mp4"/>
-                </video> : <img alt={projects.title} src={projectImage} />}
-                
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
-                  </div>
+              {
+                projects.video ? 
+                  (projects.no_hover ? 
+                    <video className="project-video" autoPlay loop muted>
+                      <source src={projectImage} type="video/mp4"/>
+                    </video> :
+                    <HoverVideoPlayer videoSrc={projectImage} hoverTarget={() => document.getElementById("overlay-" + i)}/>
+                  ) :
+                  <img alt={projects.title} src={projectImage} />
+              }
+              <div className="overlay" id={"overlay-" + i}>
+                <div className="portfolio-item-meta">
+                  <h5>{projects.title}</h5>
+                  <p>{projects.category}</p>
                 </div>
+              </div>
               <div className="link-icon"><i className="fa fa-link"></i></div>
             </a>
           </div>
