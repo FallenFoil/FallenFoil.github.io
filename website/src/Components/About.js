e3628bc99a479344e4946008f4ec9da414156f6a
Luis Macedo <luisjrsm@hotmail.com>
2021-09-06 15:26:14 +0100

    Fix age calculation

--- website/src/Components/About.js
+++ website/src/Components/About.js
@@ -6,6 +6,16 @@ class About extends Component {
     if(this.props.data){
       var profilepic= "images/"+this.props.data.image;
       var bios = this.props.data.bio.map(function(bio){
+         if(bio.includes("{age}")){
+            var current_date = new Date();
+            var age = current_date.getFullYear() - 1998;
+
+            if(current_date.getMonth() <= 1 && current_date.getDate() < 10){
+               age--;
+            }
+
+            return <p className="bio">{bio.replace("{age}", age)}</p>
+         }
          return <p className="bio">{bio}</p>
       })
       var resumeDownload = this.props.data.resumedownload;
