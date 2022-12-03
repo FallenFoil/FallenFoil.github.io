c23c5a7136439601ed8108cf3a20c1e5271aa858
Luis Macedo <luisjrsm@hotmail.com>
2022-12-03 23:30:19 +0000

    New Build

--- website/src/Components/Fibo.js
+++ website/src/Components/Fibo.js
@@ -1,9 +1,77 @@
 import React, { Component } from 'react';
+import calculate from '../fibo'
+
+const MAX_RAND = 1001
 
 class Fibo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      start: 0,
+      size: 0,
+      result: 0,
+      random: Math.floor(Math.random() * MAX_RAND)
+    };
+    this.changeStart = this.changeStart.bind(this);
+    this.changeSize = this.changeSize.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.newNumber = this.newNumber.bind(this);
+    this.useNumber = this.useNumber.bind(this);
+  }
+
+  changeStart(event) {
+    this.setState({start: event.target.value});
+  }
+
+  changeSize(event) {
+    this.setState({size: event.target.value});
+  }
+
+  handleSubmit(event) {
+    const result = calculate(this.state.start, this.state.size)
+    this.setState({result: result});
+    event.preventDefault();
+  }
+
+  newNumber(){
+    this.setState({random: Math.floor(Math.random() * MAX_RAND)});
+  }
+
+  useNumber(){
+    this.setState({start: this.state.random});
+  }
+
   render() {
     return (
-      <div>test</div>
+      <div
+        style={{
+          "margin": "auto",
+          "marginTop": "10%",
+          "display": "flex",
+          "width": "30%",
+          "flexDirection": "column"
+        }}
+      >
+        <div style={{"color": "white"}}>
+          <p>Random Number: <span style={{"fontWeight": "bold", "fontSize": "1.5em"}}>{this.state.random}</span></p>
+          <button onClick={this.newNumber}>New Number</button>
+          <button onClick={this.useNumber} style={{"marginLeft": "1em"}}>Use Number</button>
+        </div>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label style={{"color": "white"}}>
+              Start:
+              <input style={{"width":"30%", "marginLeft": "1em"}} type="number" value={this.state.start} onChange={this.changeStart}/>
+            </label>
+            <label style={{"color": "white"}}>
+              Size:
+              <input style={{"width":"30%", "marginLeft": "1.6em"}} type="number" value={this.state.size} onChange={this.changeSize}/>
+            </label>
+            <input style={{"width":"100%", "height": "30%"}} type="submit" value="Submit"/>
+            <p style={{"color": "white"}}>Result: <span style={{"fontWeight": "bold", "fontSize": "2em"}}>{this.state.result}</span></p>
+          </form>
+        </div>
+      </div>
     );
   }
 }
--- website/src/fibo.js
+++ website/src/fibo.js
@@ -1,18 +1,36 @@
 function fibo(start) {
-    if(start === 0) return 0
-    if(start === 1) return 1
-    return fibo(start-1) + fibo(start-2)
+  if(start === 0) return 0
+  if(start === 1) return 1
+  return fibo(start-1) + fibo(start-2)
 }
 
-function calculate(start, size) {
-    let res = ''
-    let i = start
-    while(res.length() < size) {
-        res += fibo(i)
-        i++
-    }
-    
-    if(res.length > size) {
-        res = res.substring(0, size)
-    }
+function fibo2(num) {
+  let x=1, y=0, temp
+
+  while(num >= 0){
+    temp=x;
+    x=x+y;
+    y=temp;
+    num--;
+  }
+
+  return y;
+}
+
+export default function calculate(start, size) {
+  let res = ''
+  let i = start
+  while(res.length < size) {
+    let x = fibo2(i)
+    console.log(x)
+    res += x
+    res = res.replace('.','')
+    i++
+  }
+  
+  if(res.length > size) {
+    res = res.substring(0, size)
+  }
+
+  return res
 }
