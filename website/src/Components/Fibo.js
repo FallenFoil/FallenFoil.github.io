c5bfb36ebb6d1679a86d3a3b813a316f29ca89f0
FallenFoil <luisjrsm@hotmail.com>
2022-12-04 15:31:08 +0000

    New Changes

--- website/src/Components/Fibo.js
+++ website/src/Components/Fibo.js
@@ -43,37 +43,37 @@ class Fibo extends Component {
 
   render() {
     return (
-      <div
-        id="fibo"
-        style={{
-          "display": "none",
-          "margin": "auto",
-          "marginTop": "10%",
-          "display": "flex",
-          "width": "30%",
-          "flexDirection": "column"
-        }}
-      >
-        <div style={{"color": "white"}}>
-          <p>Random Number: <span style={{"fontWeight": "bold", "fontSize": "1.5em"}}>{this.state.random}</span></p>
-          <button onClick={this.newNumber}>New Number</button>
-          <button onClick={this.useNumber} style={{"marginLeft": "1em"}}>Use Number</button>
+      <section id="fibo" style={{"display": "none"}}>
+        <div
+          style={{
+            "margin": "auto",
+            "marginTop": "10%",
+            "display": "flex",
+            "width": "30%",
+            "flexDirection": "column"
+          }}
+        >
+          <div style={{"color": "white"}}>
+            <p>Random Number: <span style={{"fontWeight": "bold", "fontSize": "1.5em"}}>{this.state.random}</span></p>
+            <button onClick={this.newNumber}>New Number</button>
+            <button onClick={this.useNumber} style={{"marginLeft": "1em"}}>Use Number</button>
+          </div>
+          <div>
+            <form onSubmit={this.handleSubmit}>
+              <label style={{"color": "white"}}>
+                Start:
+                <input style={{"width":"30%", "marginLeft": "1em"}} type="number" value={this.state.start} onChange={this.changeStart}/>
+              </label>
+              <label style={{"color": "white"}}>
+                Size:
+                <input style={{"width":"30%", "marginLeft": "1.6em"}} type="number" value={this.state.size} onChange={this.changeSize}/>
+              </label>
+              <input style={{"width":"100%", "height": "30%"}} type="submit" value="Submit"/>
+              <p style={{"color": "white"}}>Result: <span style={{"fontWeight": "bold", "fontSize": "2em"}}>{this.state.result}</span></p>
+            </form>
+          </div>
         </div>
-        <div>
-          <form onSubmit={this.handleSubmit}>
-            <label style={{"color": "white"}}>
-              Start:
-              <input style={{"width":"30%", "marginLeft": "1em"}} type="number" value={this.state.start} onChange={this.changeStart}/>
-            </label>
-            <label style={{"color": "white"}}>
-              Size:
-              <input style={{"width":"30%", "marginLeft": "1.6em"}} type="number" value={this.state.size} onChange={this.changeSize}/>
-            </label>
-            <input style={{"width":"100%", "height": "30%"}} type="submit" value="Submit"/>
-            <p style={{"color": "white"}}>Result: <span style={{"fontWeight": "bold", "fontSize": "2em"}}>{this.state.result}</span></p>
-          </form>
-        </div>
-      </div>
+      </section>
     );
   }
 }
